d4418617d9caedf3eacdc68845753b296b0e55d2
Jessamyn Malia <jessamynmalia@Jessamyns-MacBook-Pro.local>
2019-04-12 19:31:30 -0400

    expanded pic class 1

--- app/src/main/java/com/example/medicationbox/Picture.java
+++ app/src/main/java/com/example/medicationbox/Picture.java
@@ -10,4 +10,28 @@ public class Picture {
         this.user = user;
         this.pic = pic;
     }
+
+    public String getPicID() {
+        return picID;
+    }
+
+    public void setPicID(String picID) {
+        this.picID = picID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public byte[] getPic() {
+        return pic;
+    }
+
+    public void setPic(byte[] pic) {
+        this.pic = pic;
+    }
 }
