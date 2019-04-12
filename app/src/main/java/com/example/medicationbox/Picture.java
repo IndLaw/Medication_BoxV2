a8cc20aa69009862a04c99ca6f6e0c38dd1209d6
Jessamyn Malia <jessamynmalia@Jessamyns-MacBook-Pro.local>
2019-04-12 19:30:45 -0400

    expanded pic class

--- app/src/main/java/com/example/medicationbox/Picture.java
+++ app/src/main/java/com/example/medicationbox/Picture.java
@@ -2,8 +2,12 @@ package com.example.medicationbox;
 
 public class Picture {
     String picID;
+    User user;
+    byte[] pic;
 
-    public Picture(String picID) {
+    public Picture(String picID, User user, byte[] pic) {
         this.picID = picID;
+        this.user = user;
+        this.pic = pic;
     }
 }
