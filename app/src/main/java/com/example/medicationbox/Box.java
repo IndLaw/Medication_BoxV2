ea24b4a02ebef22c2ea0937398551eb3575ccfda
Amber Brownstein <amberbrownstein@gmail.com>
2019-04-12 17:06:10 -0400

    fixed layout for delete payment page

---
+++ app/src/main/java/com/example/medicationbox/Box.java
@@ -0,0 +1,11 @@
+package com.example.medicationbox;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Box {
+    private ArrayList<Product> p = new ArrayList<>();
+    private Date nextShipment;
+
+
+}
