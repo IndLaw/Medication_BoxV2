b0b9ee6c38a004bbf975dca568f76fa73f0e2401
Jessamyn Malia <jessamynmalia@mc-wifi-10-215-130-18.wireless.pitt.edu>
2019-04-12 21:38:57 -0400

    stuff the second

--- app/src/main/java/com/example/medicationbox/LoginActivity.java
+++ app/src/main/java/com/example/medicationbox/LoginActivity.java
@@ -18,7 +18,12 @@ import android.widget.Toast;
 
 import com.example.medicationbox.helper.Functions;
 import com.example.medicationbox.helper.SessionManager;
-
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import android.support.annotation.NonNull;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -216,5 +221,25 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
         send.start();
+    }
+
+    private void login(String email, String password)
+    {
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Signin", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
