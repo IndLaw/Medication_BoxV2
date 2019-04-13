5dc65aafdfafb9e897a238c2061ccbe8082d52b5
Alicia Hu <37424206+alicia-hu@users.noreply.github.com>
2019-04-12 23:37:12 -0400

    Update UserSingleton.java

--- app/src/main/java/com/example/medicationbox/UserSingleton.java
+++ app/src/main/java/com/example/medicationbox/UserSingleton.java
@@ -1,4 +1,25 @@
 package com.example.medicationbox;
 
 public class UserSingleton {
+    private static UserSingleton instance;
+
+    // Global variable
+    private User currentUser;
+
+    // Restrict the constructor from being instantiated
+    private UserSingleton(){}
+
+    public void setUser(User user){
+        this.currentUser = user;
+    }
+    public User getUser(){
+        return this.currentUser;
+    }
+
+    public static synchronized UserSingleton getInstance(){
+        if(instance==null){
+            instance=new UserSingleton();
+        }
+        return instance;
+    }
 }
