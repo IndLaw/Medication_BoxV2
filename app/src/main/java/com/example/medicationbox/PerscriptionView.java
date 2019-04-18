035d558f06c879e90186512cdc80accc565b7e5b
Amber Brownstein <amberbrownstein@gmail.com>
2019-04-18 00:55:29 -0400

    ?????

--- app/src/main/java/com/example/medicationbox/PerscriptionView.java
+++ app/src/main/java/com/example/medicationbox/PerscriptionView.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -37,7 +38,7 @@ public class PerscriptionView extends AppCompatActivity {
         medicationname = findViewById(R.id.medsname);
         dosedescription = findViewById(R.id.perscripdose);
         quantity = findViewById(R.id.perquantity);
-        Button home = findViewById(R.id.PerscripEnterHome);
+        FloatingActionButton home = findViewById(R.id.PerscripEnterHome);
 
 
         home.setOnClickListener(new View.OnClickListener() {
